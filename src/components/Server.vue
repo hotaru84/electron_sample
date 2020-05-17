dbd3a8382d12b8a224730ab8198c25a8c018fa43
hiroomi ohori <tkgyb713@yahoo.co.jp>
2020-05-17 11:49:33 +0900

    add qr code link

--- src/components/Server.vue
+++ src/components/Server.vue
@@ -11,13 +11,14 @@
         <v-btn @click="cancel">CANCEL</v-btn>
       </v-col>
       <v-col>
-        <v-btn color="primary" @click="done">DONE</v-btn>
+        <v-btn color="primary" @click="start">START</v-btn>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+import { isNullOrUndefined } from 'util'
 const qrcode = require('qrcode')
 const http = require('http')
 const fs = require('fs')
@@ -33,37 +34,51 @@ export default {
     },
     cancel(){
       this.server.close()
+      this.server = null
       this.$emit('cancel')
     },
     webserver() {
       return http.createServer((req,res)=>{
-        const files = this.$store.getters.files
+        const files = this.files
           .filter(f=>f.name == req.url.replace('/',''))
         if(files.length > 0){
           window.console.log("found:" + files[0])
-          const rs = fs.createReadStream(files[0].path,'utf8')
-          res.writeHead(200,{'Content-Type':files[0].type});
+          const rs = fs.createReadStream(files[0].path)
+          res.writeHead(200,{
+            'Content-Type':files[0].type,
+            "Content-Length": files[0].size
+            });
           rs.pipe(res)
         } else {
           window.console.log("not found:" + req.path)
-          res.writeHead(404, {'Content-Type': 'text/plain'});
+          res.writeHead(404, {'Content-Type': 'text/plain',
+          });
           res.end('my notfound! : ' + req.path);
         }
       })
-    }
+    },
+    start(){
+      if(isNullOrUndefined(this.server)){
+        if(this.files != null && this.files.length > 0){
+          qrcode.toDataURL("http://" + this.ip + ":3000/" + this.files[0].name)
+          .then(url=>{
+            this.qrurl = url
+          })
+        }
+        this.server = this.webserver()
+        this.server.listen(3000,this.$store.getters.ipaddr)
+      } else {
+        this.server.close()
+        this.server = null
+      }
+    },
   },
-  mounted(){
-    qrcode.toDataURL("test")
-    .then(url=>{
-      this.qrurl = url
-    })
-    
-    this.server = this.webserver()
-    this.server.listen(3000,this.$store.getters.ipaddr)
-  },
+  computed:{
+    files() {return this.$store.getters.files},
+    ip() {return this.$store.getters.ipaddr},
+  }, 
   beforeDestroy() {
     this.server.close()
-    >window.console.log("close")
   }
 }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,30 +5,13 @@
       <v-divider></v-divider>
       <v-stepper-step :complete="e1 > 2" step="2">Name of step 2</v-stepper-step>
       <v-divider></v-divider>
-      <v-stepper-step step="3">Name of step 3</v-stepper-step>
     </v-stepper-header>
     <v-stepper-items>
       <v-stepper-content step="1">
         <setup @done="e1 = 2"/>
       </v-stepper-content>
       <v-stepper-content step="2">
-        <server @done="e1 = 3" @cancel="e1 = 1"/>
-      </v-stepper-content>
-      <v-stepper-content step="3">
-        <v-card
-          class="mb-12"
-        >
-
-        </v-card>
-
-        <v-btn
-          color="primary"
-          @click="e1 = 1"
-        >
-          Continue
-        </v-btn>
-
-        <v-btn text>Cancel</v-btn>
+        <server @done="e1 = 1" @cancel="e1 = 1"/>
       </v-stepper-content>
     </v-stepper-items>
   </v-stepper>
